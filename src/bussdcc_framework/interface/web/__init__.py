a7b0fee230b92bf4a336ad30c42d9d3ec4c994f1
Joshua B. Bussdieker <jbussdieker@gmail.com>
2026-03-20 22:08:41 -0500

    feat(web): expose FlaskApp in web interface
    - Add FlaskApp to the `__all__` list in `web/__init__.py`
    - Import FlaskApp in `web.py` for broader accessibility

--- src/bussdcc_framework/interface/web/__init__.py
+++ src/bussdcc_framework/interface/web/__init__.py
@@ -1,7 +1,9 @@
+from .base import FlaskApp
 from .interface import WebInterface
 from .context import current_ctx, emit
 
 __all__ = [
+    "FlaskApp",
     "WebInterface",
     "current_ctx",
     "emit",
--- src/bussdcc_framework/web.py
+++ src/bussdcc_framework/web.py
@@ -1,7 +1,8 @@
-from .interface.web import WebInterface, current_ctx, emit
+from .interface.web import FlaskApp, WebInterface, current_ctx, emit
 from .interface.web.protocol import WebPlugin
 
 __all__ = [
+    "FlaskApp",
     "WebInterface",
     "WebPlugin",
     "current_ctx",
